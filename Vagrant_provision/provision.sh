8aaaecfa774200dcfc362e8f424b3b1d6a14cf30
Ash Isbitt <AIsbitt@tcadmins-air-2.home>
2020-04-15 15:59:45 +0100

    fixed issue with berksfile in dev env

--- Vagrant_provision/provision.sh
+++ Vagrant_provision/provision.sh
@@ -3,8 +3,8 @@ sudo apt-get upgrade -y
 
 sudo apt-get install python3.7 -y
 sudo apt-get install python3-pip -y
-sudo pip3 install -r /home/ubuntu/Python_Environment_Project/Starter_Code/requirements.txt
+sudo pip3 install -r /home/vagrant/Starter_Code/requirements.txt
 
-sudo mkdir Downloads
+sudo mkdir -p Downloads
 
-sudo chmod 777 ~/Downloads/
+sudo chmod 0777 ~/Downloads/
